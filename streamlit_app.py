13cca7c767bda58b57c367b764b3e057c03d6c50
honeysnow <120142975+honeysnowjeanette@users.noreply.github.com>
2022-12-11 22:52:13 -0600

    Update streamlit_app.py
    added panda call and read csv file function

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,7 @@ streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Range-Free Egg')
 streamlit.text('🥑🍞Avacado Toast')
 streamlit.header('🍌🍓Build Your Own Smoothie🥝🍇')
+
+import Pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(myfruitlist)
