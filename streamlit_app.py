2bdb7bf222ad5b9ce0e004c68f3df443f9b34777
honeysnow <120142975+honeysnowjeanette@users.noreply.github.com>
2022-12-11 22:56:01 -0600

    Update streamlit_app.py
    corrected typo in myfruitlist

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,4 @@ streamlit.header('🍌🍓Build Your Own Smoothie🥝🍇')
 
 import Pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(myfruitlist)
+streamlit.dataframe(my_fruit_list)
