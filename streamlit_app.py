20abc7aa95e912f80cdf52876a3615901d743f57
honeysnow <120142975+honeysnowjeanette@users.noreply.github.com>
2022-12-11 22:17:05 -0600

    Update streamlit_app.py
    change to favorites
    Added new menu items and grape banana emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breafast Menu')
+streamlit.header('Breafast Favorites')
 
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Range-Free Egg')
 streamlit.text('🥑🍞Avacado Toast')
+streamlit.header('🍌🍓Build Your Own Smoothie🥝🍇')
