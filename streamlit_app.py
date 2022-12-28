3d822444e3f514f2c0a911a4dc1fbf3a1d1ba09a
honeysnow <120142975+honeysnowjeanette@users.noreply.github.com>
2022-12-27 23:17:02 -0600

    Update streamlit_app.py
    remove statements to test Panda line

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,3 @@ streamlit.text('🥑🍞Avacado Toast')
 streamlit.header('🍌🍓Build Your Own Smoothie🥝🍇')
 
 import Pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
