a5b73c404fcded592dbf9bbd1ec616cd7db8ddac
honeysnow <120142975+honeysnowjeanette@users.noreply.github.com>
2022-12-27 23:23:45 -0600

    Update streamlit_app.py
    added 3 new lines with panda

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,6 @@ streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Range-Free Egg')
 streamlit.text('🥑🍞Avacado Toast')
 streamlit.header('🍌🍓Build Your Own Smoothie🥝🍇')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2-amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
