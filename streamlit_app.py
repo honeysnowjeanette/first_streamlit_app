c2c905e548d9f46009af63cdd4afb4a5c641dd7a
honeysnow <120142975+honeysnowjeanette@users.noreply.github.com>
2023-01-12 11:57:20 -0600

    added variable ussage "kiwi

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,10 +25,10 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "Kiwi")
+#streamlit.text(fruityvice_response.json()) 
 
 # Flatten data into a table
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output to the screen
+# output to the screen as a table
 streamlit.dataframe(fruityvice_normalized)
